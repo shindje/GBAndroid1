2c2ca45d6836b206b74862180267492e708aeafc
shindje <aleks.shindje@gmail.com>
2020-08-04 18:50:31 +0300

    Homework 4 - Intent

--- app/src/main/java/com/example/goodweather/MainActivity.java
+++ app/src/main/java/com/example/goodweather/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.goodweather;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView windTextView;
-    TextView pressureTextView;
-    CheckBox addInfoCheckBox;
-    TextView temperature;
-    Button updateDataButton;
+    static String[][] validCityNames = {{"Москва", "moscow"}, {"Moscow", "moscow"},
+                                        {"Лондон", "london"}, {"London", "london"},
+                                        {"Париж", "paris"}, {"Paris", "paris"},
+                                        {"Нью-Йорк", "new-york"}, {"New York", "new-york"}};
 
-    final String temperatureKey = "temperature";
+    TextView windTextView, pressureTextView, cityTextView, temperatureTextView, windValueTextView,
+            pressureValueTextView;
+    CheckBox addInfoCheckBox;
+    Button updateDataButton, selectCityBtn, yandexWeatherBtn;
+
+    final static String temperatureKey = "temperature";
+    final static String cityKey = "city";
+    final static String windValueKey = "windValue";
+    final static String pressureValueKey = "pressureValue";
+    final static int cityRequestCode = 100;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,28 +80,39 @@ public class MainActivity extends AppCompatActivity {
         windTextView = findViewById(R.id.windTextView);
         pressureTextView = findViewById(R.id.pressureTextView);
         addInfoCheckBox = findViewById(R.id.addInfoCheckBox);
-        temperature = findViewById(R.id.temperature);
+        temperatureTextView = findViewById(R.id.temperature);
         updateDataButton = findViewById(R.id.updateDataButton);
+        selectCityBtn = findViewById(R.id.selectCityBtn);
+        cityTextView = findViewById(R.id.cityTextView);
+        windValueTextView = findViewById(R.id.windValueTextView);
+        pressureValueTextView = findViewById(R.id.pressureValueTextView);
+        yandexWeatherBtn = findViewById(R.id.yandexWeatherBtn);
     }
 
     private void setOnClickListeners(){
-        addInfoCheckBox.setOnClickListener((View view) -> {
-            if (((CheckBox)view).isChecked()) {
-                windTextView.setVisibility(View.VISIBLE);
-                pressureTextView.setVisibility(View.VISIBLE);
-            } else {
-                windTextView.setVisibility(View.INVISIBLE);
-                pressureTextView.setVisibility(View.INVISIBLE);
-            }
+        addInfoCheckBox.setOnClickListener(view -> setViewsVisible());
+        updateDataButton.setOnClickListener(view -> updateData());
+        selectCityBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CitySelector.class);
+            intent.putExtra(cityKey, cityTextView.getText().toString());
+            intent.putExtra(windValueKey, windValueTextView.getText().toString());
+            intent.putExtra(pressureValueKey, pressureValueTextView.getText().toString());
+            startActivityForResult(intent, cityRequestCode);
         });
-        updateDataButton.setOnClickListener((View view) -> {
-            updateData();
+        yandexWeatherBtn.setOnClickListener(view -> {
+            String cityForURL = getCityforURL();
+            Uri uri = Uri.parse("https://yandex.ru/pogoda/" + cityForURL);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
         });
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putString(temperatureKey, temperature.getText().toString());
+        outState.putString(cityKey, cityTextView.getText().toString());
+        outState.putString(temperatureKey, temperatureTextView.getText().toString());
+        outState.putString(windValueKey, windValueTextView.getText().toString());
+        outState.putString(pressureValueKey, pressureValueTextView.getText().toString());
         super.onSaveInstanceState(outState);
         onLifeCycle("onSaveInstanceState");
     }
@@ -99,24 +120,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        temperature.setText(savedInstanceState.getString(temperatureKey));
+        cityTextView.setText(savedInstanceState.getString(cityKey));
+        temperatureTextView.setText(savedInstanceState.getString(temperatureKey));
+        windValueTextView.setText(savedInstanceState.getString(windValueKey));
+        pressureValueTextView.setText(savedInstanceState.getString(pressureValueKey));
         setViewsVisible();
         onLifeCycle("onRestoreInstanceState");
     }
 
     private void updateData(){
-        temperature.setText("+" + ((int)(Math.random()*5) + 20));
+        temperatureTextView.setText("+" + ((int)(Math.random()*5) + 20));
     }
 
     private void setViewsVisible() {
         boolean showAddInfo = addInfoCheckBox.isChecked();
         windTextView.setVisibility(showAddInfo? View.VISIBLE: View.INVISIBLE);
         pressureTextView.setVisibility(showAddInfo? View.VISIBLE: View.INVISIBLE);
+        windValueTextView.setVisibility(showAddInfo? View.VISIBLE: View.INVISIBLE);
+        pressureValueTextView.setVisibility(showAddInfo? View.VISIBLE: View.INVISIBLE);
     }
 
     private void onLifeCycle(String action) {
         String text = getString(R.string.app_name) + " " + action;
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
         Log.d("onLifeCycle", text);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == cityRequestCode && resultCode == RESULT_OK) {
+            if (data != null) {
+                cityTextView.setText(data.getStringExtra(cityKey));
+                windValueTextView.setText(data.getStringExtra(windValueKey));
+                pressureValueTextView.setText(data.getStringExtra(pressureValueKey));
+            }
+        }
+    }
+
+    private String getCityforURL() {
+        String curCityName = cityTextView.getText().toString();
+        for (String[] names: validCityNames) {
+            if (names[0].equals(curCityName))
+                return names[1];
+        }
+        return "";
     }
 }
